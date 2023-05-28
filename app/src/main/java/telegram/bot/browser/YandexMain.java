8aa0d2540ffe6a6542d50b626190abe4cbb3ff5e
mychko <v.mychko@premiumitsolution.com>
2023-05-28 17:36:41 +0300

    --disable-dev-shm-usage

--- app/src/main/java/telegram/bot/browser/YandexMain.java
+++ app/src/main/java/telegram/bot/browser/YandexMain.java
@@ -33,8 +33,8 @@ public class YandexMain {
     public void savePricesFromBrowser() {
         Configuration.headless = true;
         Configuration.webdriverLogsEnabled = true;
-        System.setProperty("webdriver.chrome.logfile", "./chromedriver.log");
         System.setProperty("webdriver.chrome.verboseLogging", "true");
+        System.setProperty("chromeoptions.args", "--disable-dev-shm-usage");
         open(url);
         SelenideElement container = $("article.currency-rates__container-3P");
         container.shouldBe(visible);
